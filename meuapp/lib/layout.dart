cd187050d10081f2015d83ab80c6b714d9be56ac
Carlos Gabriel <gabrieldfm13@gmail.com>
2019-10-26 16:05:53 -0300

    ajustes texto

--- meuapp/lib/layout.dart
+++ meuapp/lib/layout.dart
@@ -4,96 +4,31 @@ import 'pages/home.dart';
 import 'pages/about.dart';
 import 'pages/settings.dart';
 
-class Layout{
-
-  static final pages = [
-    HomePage.tag,
-    AboutPage.tag,
-    SettingsPage.tag
-  ];
+class Layout {
+  static final pages = [HomePage.tag, AboutPage.tag, SettingsPage.tag];
 
   static int currItem = 0;
 
-  static Scaffold getContent(BuildContext context, content){
-
+  static Scaffold getContent(BuildContext context, content) {
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Layout.primary(),
         title: Center(
           child: Text('Lista compras'),
         ),
-        actions: <Widget>[
-          GestureDetector(
-            onTap: (){
-              showDialog(
-                context: context,
-                //barrierDismissible: false,
-                builder: (BuildContext ctx){
-
-                  final input = TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Nome',
-                      contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5)
-                      )
-                    ),
-                  );
-
-                  return AlertDialog(
-                    title: Text('Nome da lista'),
-                    content: SingleChildScrollView(
-                      child: ListBody(
-                        children: <Widget>[
-                          input
-                        ],
-                      ),
-                    ),
-                    actions: <Widget>[
-                      RaisedButton(
-                        color: secondary(),
-                        child: Text('Cancelar', style: TextStyle(color: light())),
-                        onPressed: (){
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      RaisedButton(
-                        color: primary(),
-                        child: Text('Adicionar', style: TextStyle(color: light())),
-                        onPressed: (){
-                          Navigator.of(context).pop();
-                        },
-                      )
-                    ],
-                  );
-                }
-
-              );
-              print('teste');
-            },
-            child: Icon(Icons.add),
-          ),
-          Padding(padding: EdgeInsets.only(right: 20))
-        ],
+        actions: _getActions(context),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currItem,
         fixedColor: primary(),
         items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.info), title: Text('Sobre')),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home')
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            title: Text('Sobre')
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Configurações')
-          )
+              icon: Icon(Icons.settings), title: Text('Configurações'))
         ],
-        onTap: (int i){
+        onTap: (int i) {
           currItem = i;
           Navigator.of(context).pushNamed(pages[currItem]);
         },
@@ -102,14 +37,76 @@ class Layout{
     );
   }
 
+  static List<Widget> _getActions(context) {
+    List<Widget> items = List<Widget>();
 
-  static Color primary([double opacity = 1]) => Color.fromRGBO(62, 63, 89, opacity);
-  static Color secondary([double opacity = 1]) => Color.fromRGBO(111, 168, 191, opacity);
-  static Color light([double opacity = 1]) => Color.fromRGBO(242, 234, 228, opacity);
-  static Color dark([double opacity = 1]) => Color.fromRGBO(51, 51, 51, opacity);
+    TextEditingController _controller = TextEditingController();
 
-  static Color danger([double opacity = 1]) => Color.fromRGBO(217, 74, 74, opacity);
-  static Color success([double opacity = 1]) => Color.fromRGBO(6, 166, 59, opacity);
-  static Color info([double opacity = 1]) => Color.fromRGBO(0, 122, 166, opacity);
-  static Color warning([double opacity = 1]) => Color.fromRGBO(166, 134, 0, opacity);
+    items.add(GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext ctx) {
+              final input = TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(
+                    hintText: 'Nome',
+                    contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5))),
+              );
+
+              return AlertDialog(
+                title: Text('Nome da lista'),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[input],
+                  ),
+                ),
+                actions: <Widget>[
+                  RaisedButton(
+                    color: secondary(),
+                    child: Text('Cancelar', style: TextStyle(color: light())),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  RaisedButton(
+                    color: primary(),
+                    child: Text('Adicionar', style: TextStyle(color: light())),
+                    onPressed: () {
+                      print(_controller.text);
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              );
+            });
+      },
+      child: Icon(Icons.add),
+    ));
+
+    items.add(Padding(padding: EdgeInsets.only(right: 20)));
+
+    return items;
+  }
+
+  static Color primary([double opacity = 1]) =>
+      Color.fromRGBO(62, 63, 89, opacity);
+  static Color secondary([double opacity = 1]) =>
+      Color.fromRGBO(150, 150, 150, opacity);
+  static Color light([double opacity = 1]) =>
+      Color.fromRGBO(242, 234, 228, opacity);
+  static Color dark([double opacity = 1]) =>
+      Color.fromRGBO(51, 51, 51, opacity);
+
+  static Color danger([double opacity = 1]) =>
+      Color.fromRGBO(217, 74, 74, opacity);
+  static Color success([double opacity = 1]) =>
+      Color.fromRGBO(6, 166, 59, opacity);
+  static Color info([double opacity = 1]) =>
+      Color.fromRGBO(0, 122, 166, opacity);
+  static Color warning([double opacity = 1]) =>
+      Color.fromRGBO(166, 134, 0, opacity);
 }
