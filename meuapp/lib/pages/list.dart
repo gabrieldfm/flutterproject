89b39c11f0f055d7b7150c7a797cac15ce43b4d0
Carlos Gabriel <gabrieldfm13@gmail.com>
2019-11-03 20:55:27 -0300

    totais lista

--- meuapp/lib/pages/list.dart
+++ meuapp/lib/pages/list.dart
@@ -12,26 +12,59 @@ class ListPage extends StatefulWidget {
 class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
-    final content = Column(
-      children: <Widget>[
-        SizedBox(
-          child: TextFormField(
-            decoration: InputDecoration(hintText: 'Pesquisar'),
+    final content = SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            child: TextFormField(
+              decoration: InputDecoration(hintText: 'Pesquisar'),
+            ),
           ),
-        ),
-        Container(
-          color: Colors.blueGrey,
-          height: MediaQuery.of(context).size.height - 300,
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.adjust),
-                title: Text('Nome do item'),
-              )
-            ],
+          Container(
+            color: Colors.blueGrey,
+            height: MediaQuery.of(context).size.height - 300,
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.adjust),
+                  title: Text('Nome do item'),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+          Container(
+            color: Colors.deepOrange,
+            height: 80,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Total de itens: 1'),
+                      Text('Já adquirido: 0')
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.indigo,
+                  width: MediaQuery.of(context).size.width /2,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Total: R\$ 15,00', style: TextStyle(fontSize: 24),),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
 
     return Layout.getContent(context, content, false);
